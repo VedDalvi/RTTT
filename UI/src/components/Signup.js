913690331c0c8f7fbb8d71ddbf05ae7989f3149d
Rutij Navelkar <navelkarrutij@gmail.com>
2024-03-20 11:17:01 +0530

    Signup.js and App.css changed

--- UI/src/components/Signup.js
+++ UI/src/components/Signup.js
@@ -5,26 +5,21 @@ export default function Login() {
         <div className="main">
             <div className="sub-main">
                 <div className="title">
-                    <h1> Sign-Up</h1>
+                    <h1>Sign-Up</h1>
                 </div>
 
-                <div className="textt">
-                    <div>
-                        <input type=" text " placeholder="User Name " className=" Name" />
-                    </div>
+                <div className="textt">                    
+                    <input type=" text " placeholder="User Name " className="input-signup" />                    
                     <br/>
-                    <div>
-                        <input type="email" placeholder="Email " className="Name"/>
-                    </div>
-                    <div className="second-input" />
-                    <div>
-                        <input type=" password " placeholder="Password " className=" Name" />
-                    </div>
+                    <br/>
+                    <input type="email" placeholder="Email " className="input-signup"/>
+                    <br/>
+                    <br/>                    
+                    <input type=" password " placeholder="Password " className="input-signup"/>                    
                 </div>
-
-                    <div className="Login-button">
-                        <button className="button2"> Register </button>
-                    </div>
+                <div className="Login-button">
+                    <button className="button2"> Register </button>
+                </div>
                     <div className="link1">
                         <p>
                             Already have an account? <a href="/">Sign in</a>
