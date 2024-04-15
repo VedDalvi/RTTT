807337aa6d6e642f670756c3532a1ae50fe64bca
Rutij Navelkar <navelkarrutij@gmail.com>
2024-04-15 15:28:41 +0530

    Validation for uploaded files.

--- UI/src/components/translation/VidCaption.js
+++ UI/src/components/translation/VidCaption.js
@@ -25,20 +25,35 @@ function VideoTranslation() {
     setIsDragging(false);
 
     const file = e.dataTransfer.files[0];
-    setSelectedVideo(file);
-    submitForm(e);
+    validateAndSetVideo(file);
   };
 
   const handleVideoChange = (e) => {
     e.preventDefault();
-
+  
     const file = e.target.files[0];
-    setSelectedVideo(file);
-    submitForm(e);
+    validateAndSetVideo(file);
+  };
+
+  const validateAndSetVideo = (file) => {
+    if (file) {
+      const validFormats = ['.mp4'];
+      const extension = file.name.substring(file.name.lastIndexOf('.')).toLowerCase();
+      if (validFormats.includes(extension)) {
+        setSelectedVideo(file);
+        console.log("File Submited",file.name);
+        submitForm();
+      } else {
+        console.log("Error");
+        alert('Invalid file format. Please upload only .mp4 files.');
+      }
+    }
   };
 
   const submitForm = (event) => {
-    event.preventDefault();
+    if (event) {
+      event.preventDefault();
+    }
   };
 
   return (
@@ -49,7 +64,7 @@ function VideoTranslation() {
             <form ref={formRef}>
               <label align="center"><br />Drag and Drop <br />or</label>
               <label className="iolabel" htmlFor="videoupld" align="center">select a video to upload.</label>
-              <input type="file" id="videoupld" style={{ display: "none" }} onChange={handleVideoChange} accept=".mp4, .mkv, .avi" />
+              <input type="file" id="videoupld" style={{ display: "none" }} onChange={handleVideoChange} accept=".mp4" />
             </form>
             {selectedVideo && (
               <>
