69b73213ae2408249618b9fe651529d262b75fde
Rutij Navelkar <navelkarrutij@gmail.com>
2024-05-06 12:30:08 +0530

    modifications to index.js file for file upload and OCR script activation to analyze the file.

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const multer  = require('multer');
 const mysql = require('mysql');
 const fs = require('fs');
+const { spawn } = require('child_process');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const { register,login,deleteFile } = require("./controllers.js");
@@ -28,12 +30,48 @@ app.use(bodyParser.json());
 
 app.post('/upload/pdf', upload.single('file'), function (req, res, next) {
   console.log(req.file.filename);
-  deleteFile(process.env.uploads + "/" + req.file.filename);
+  const filePath = path.join(process.env.uploads, req.file.filename);
+
+  // Spawn a child process to execute the Python script
+  const pythonProcess = spawn('python', ['C:\\Users\\Rutij\\Desktop\\Project\\RTTT\\OCR\\Tesseract_OCR_test.py', filePath]);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python script stdout: ${data}`);
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python script stderr: ${data}`);
+  });
+
+  pythonProcess.on('close', (code) => {
+    console.log(`Python script child process exited with code ${code}`);
+    // Send response indicating success
+  });
+
+  setTimeout(()=>deleteFile(process.env.uploads + "/" + req.file.filename),7000);
   res.send(req.file.filename)
 });
 
 app.post('/upload/image', upload.single('image'), function (req, res, next) {
   console.log(req.file.filename);
+  const imagePath = path.join(process.env.uploads, req.file.filename);
+  
+  // Spawn a child process to execute the Python script for image translation
+  const pythonProcess = spawn('python', ['C:\\Users\\Rutij\\Desktop\\Project\\RTTT\\OCR\\Tesseract_OCR_test.py', imagePath]);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python script stdout: ${data}`);
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python script stderr: ${data}`);
+  });
+
+  pythonProcess.on('close', (code) => {
+    console.log(`Python script child process exited with code ${code}`);
+    // Send response indicating success
+  });
+
   setTimeout(()=>deleteFile(process.env.uploads + "/" + req.file.filename), 7000)
   res.send(req.file.filename)
 });
