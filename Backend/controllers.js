f30f6310c42c6ad529ee0dc8d876c19cc2e127c4
VedDalvi <85603580+VedDalvi@users.noreply.github.com>
2024-05-06 17:51:43 +0530

    Add files via upload

--- Backend/controllers.js
+++ Backend/controllers.js
@@ -1,71 +1,73 @@
 const fs = require('fs');
-// const mysql = require('mysql');
+ const mysql = require('mysql');
+ require('dotenv').config();
+ 
 
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE
-// });
+ const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+ });
 
 // Connect to the database
-// connection.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting to database:', err);
-//         return;
-//     }
-//     console.log('Connected to MySQL database');
-// });
+ connection.connect((err) => {
+     if (err) {
+         console.error('Error connecting to database:', err);
+         return;
+     }
+     console.log('Connected to MySQL database');
+ });
 
 
 
-// exports.register = function (req, res) {
-//     const { username, password, email } = req.body;
+ exports.register = function (req, res) {
+     const { username, password, email } = req.body;
 
-//     // Check if the username already exists
-//     connection.query('SELECT * FROM users WHERE username = ? OR email = ?', [username, email], (err, results) => {
-//         if (err) {
-//             console.error('Error executing query:', err);
-//             res.status(500).json({ error: 'Internal Server Error' });
-//             return;
-//         }
+     // Check if the username already exists
+     connection.query('SELECT * FROM users WHERE username = ? OR email = ?', [username, email], (err, results) => {
+         if (err) {
+             console.error('Error executing query:', err);
+             res.status(500).json({ error: 'Internal Server Error' });
+             return;
+         }
 
-//         if (results.length > 0) {
-//             res.status(400).json({ error: 'Username or Email already exists' });
-//             return;
-//         }
+         if (results.length > 0) {
+             res.status(400).json({ error: 'Username or Email already exists' });
+             return;
+        }
 
 
-//         // Insert the new user into the database
-//         connection.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [username, password, email], (err) => {
-//             if (err) {
-//                 console.error('Error executing query:', err);
-//                 res.status(500).json({ error: 'Internal Server Error' });
-//                 return;
-//             }
+         // Insert the new user into the database
+         connection.query('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [username, password, email], (err) => {
+             if (err) {
+                 console.error('Error executing query:', err);
+                res.status(500).json({ error: 'Internal Server Error' });
+                 return;
+             }
 
-//             res.status(201).json({ message: 'User registered successfully' });
-//         });
-//     });
-// };
+             res.status(201).json({ message: 'User registered successfully' });
+         });
+     });
+ };
 
-// exports.login = function (req, res){
-//     const { email, password, } = req.body;
+ exports.login = function (req, res){
+     const { email, password, } = req.body;
 
-//     connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {
-//         if (err) {
-//             console.error('Error executing query:', err);
-//             res.status(500).json({ error: 'Internal Server Error' });
-//             return;
-//         }
+     connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {
+         if (err) {
+             console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+         }
 
-//         if (results.length > 0) {
-//             res.status(200).json({ message: 'Login successful' });
-//         } else {
-//             res.status(401).json({ error: 'Invalid credentials' });
-//         }
-//     });
-// };
+         if (results.length > 0) {
+             res.status(200).json({ message: 'Login successful' });
+         } else {
+            res.status(401).json({ error: 'Invalid credentials' });
+         }
+     });
+ };
 
 exports.deleteFile = (filePath) => {
     // Check if the file exists
