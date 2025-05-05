e31a2799a8f78f5537f16b347cbf4fe416144f76
Rutij Navelkar <navelkarrutij@gmail.com>
2025-05-05 21:43:32 +0530

    a bit of commenting to ProtectedRoute.js explaining what it is used for

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -1,4 +1,7 @@
 const jwt = require('jsonwebtoken');
+//Generate a JSON Web Token with a 24h duration.
+//Used to ensure only a logged in user can access the translation features on website.
+//This token is also used by the react-app in ProtectedRoute.js to ensure the previous comment.
 
 exports.generateToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '24h' });
--- UI/src/components/ProtectedRoute.js
+++ UI/src/components/ProtectedRoute.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+//This is to ensure that only a logged in user can access the main functionalities of the website.
+//That means trying to access a link such as http://localhost:3000/translate without loggin in first would
+//redirect that user to the login page.
+//Login generates a 'token' stored in local storage, which will expire after 24h as specified in the node js script to generate jwt tokens.
+//Import this file to main app.js where you have setup your react router to protect important links you wish only logged in user should access.
+
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem('token');
   if (!token) {
