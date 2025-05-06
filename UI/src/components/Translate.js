e89b8e032f3781f1c42c5253002d2495ca788c21
Rutij Navelkar <navelkarrutij@gmail.com>
2025-05-06 06:57:46 +0530

    Changes to UI for translation page(Changed background, made buttons a bit more aesthetic and also added a event listner to clear the translation screen when clicking outside it.

--- UI/src/components/Translate.js
+++ UI/src/components/Translate.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Navbar from './navbar/Navbar';
 import Cards from './cards/Cards';
 import TextTranslation from './translation/TextTranslate';
@@ -8,21 +8,31 @@ import VidTranslate from './translation/VidCaption'
 
 export default function Translate({isLoggedIn, setLoggedIn}) {
   const [selectedOption, setSelectedOption] = useState(null);
-
+  const componentRef = useRef(null);
   const handleOptionSelect = (option) => {
-    setSelectedOption(option);
+    if (componentRef.current && !componentRef.current.contains(option.target)) {
+      setSelectedOption(false);
+    } else {
+      setSelectedOption(option);
+    }
   };
 
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOptionSelect);
+    return () => {
+      document.removeEventListener('mousedown', handleOptionSelect);
+    };
+  }, []);
   return (
    <>
     <Navbar isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn}/>
-    <div className="container">
-      <Cards onSelectOption={handleOptionSelect} />
-      {selectedOption === 'Translate Text' && <TextTranslation />}
-      {selectedOption === 'Translate Files' && <FileTranslation />}
-      {selectedOption === 'Translate Image' && <ImageTranslation />}
-      {selectedOption === 'Video Captioning' && <VidTranslate />}
-    </div>
+      <div className="translate_bg">
+        <Cards onSelectOption={handleOptionSelect} />
+        {selectedOption === 'Translate Text' && <TextTranslation />}
+        {selectedOption === 'Translate Files' && <FileTranslation />}
+        {selectedOption === 'Translate Image' && <ImageTranslation />}
+        {selectedOption === 'Video Captioning' && <VidTranslate />}
+      </div>
     </>
   );
 }
